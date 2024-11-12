0ee0b5316e2c053d7569c0f004556258cc997723
Jake Salter <146293252+JakeSalter-Coder@users.noreply.github.com>
2024-11-11 21:29:53 -0600

    Update StoreUserInfo.java

--- src/application/StoreUserInfo.java
+++ src/application/StoreUserInfo.java
@@ -8,21 +8,25 @@ import login.CurrentUserSession;
 import java.sql.PreparedStatement;
 
 public class StoreUserInfo {
+	public static void main(String[] args) {
+		store_user_info(26, 120, 175, "Strength", 5, "Beginner");
+	}
+	
 	public static boolean store_user_info(int age, int height, int weight, String goals, int frequency, String level) {
-		try {
-			Connection conn = DriverManager.getConnection("jdbc:sqlite:database/java-trainer.db");
-			String insert_statement = "INSERT INTO user_personal_data (UserID, Age, Weight, Height, Goals, Freq, Level) " +
-									  "VALUES (?, ?, ?, ?, ?, ?, ?)";
-			PreparedStatement filled_insert_statement = conn.prepareStatement(insert_statement);
-			filled_insert_statement.setInt(1, CurrentUserSession.getCurrentSession().getCurrentUser().getID());
-			filled_insert_statement.setInt(2, age);
-			filled_insert_statement.setInt(3, height);
-			filled_insert_statement.setInt(4, weight);
-			filled_insert_statement.setString(5, goals);
-			filled_insert_statement.setInt(6, frequency);
-			filled_insert_statement.setString(7, level);
-			filled_insert_statement.executeUpdate();
-			return true;
+		try (Connection conn = DriverManager.getConnection("jdbc:sqlite:database/java-trainer.db")){
+				String insert_statement = "INSERT INTO user_personal_data (UserID, Age, Weight, Height, Goals, Freq, Level) " +
+										  "VALUES (?, ?, ?, ?, ?, ?, ?)";
+				try(PreparedStatement filled_insert_statement = conn.prepareStatement(insert_statement)){
+					filled_insert_statement.setInt(1, CurrentUserSession.getCurrentSession().getCurrentUser().getID());
+					filled_insert_statement.setInt(2, age);
+					filled_insert_statement.setInt(3, height);
+					filled_insert_statement.setInt(4, weight);
+					filled_insert_statement.setString(5, goals);
+					filled_insert_statement.setInt(6, frequency);
+					filled_insert_statement.setString(7, level);
+					filled_insert_statement.executeUpdate();
+					return true;
+				} 	
 		} catch(SQLException e) {
 			System.out.println(e);
 			return false;
